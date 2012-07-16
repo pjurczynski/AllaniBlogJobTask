d825c32ff0d56679a911abedbfd1313cacb43c7b
Peter Jurczyński <pjurczynski@gmail.com>
2012-07-16 11:58:12 +0200

    init commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+gem "haml", ">= 3.1.6"
+gem "haml-rails", ">= 0.3.4", :group => :development
+gem "bootstrap-sass", ">= 2.0.4.0"
+gem "therubyracer", :group => :assets, :platform => :ruby
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllaniBlog::Application.config.secret_token = 'f3912cf65c26260ec7f2941ed21067d26385119503e7b58066d4c8750016b48cace664a21da3d2590e495a2ecdf8041fd77957cd45304a8b0e702c26f18c8c62'
